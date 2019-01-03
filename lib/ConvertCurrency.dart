cedd66a44af6ae302b172c494e3707f797fc026f
Pierre <pierre.terroitin@thingthing.co>
2019-01-03 07:56:47 +0100

    init

--- lib/ConvertCurrency.dart
+++ lib/ConvertCurrency.dart
@@ -3,13 +3,23 @@ import 'package:flutter/material.dart';
 class ConvertCurrency extends StatefulWidget {
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return ConvertCurrencyState();
-  }
+  _ConvertCurrencyState createState() => _ConvertCurrencyState();
+
 }
 
-class ConvertCurrencyState extends State<ConvertCurrency> {
+class _ConvertCurrencyState extends State<ConvertCurrency> {
+
+  final _rmbValueController = TextEditingController();
+
+  double _eurValue = 0;
+  double _dolValue = 0;
+  String _value = "";
+
+  _changeRmbValue(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +27,18 @@ class ConvertCurrencyState extends State<ConvertCurrency> {
       body: Container(
         color: Colors.yellow,
         child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Text("Currency Converter", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black)),
-            ],
+          child: SizedBox.expand(
+            child: Column(
+              children: <Widget>[
+                Text("Currency Converter", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black)),
+                TextField(controller: _rmbValueController, onTap: onc(_rmbValueController.text)),
+                Text(_value)
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
